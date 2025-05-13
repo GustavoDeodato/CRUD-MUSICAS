35bd05e2aac9f617682317fb393332cdf29f1f0f
GustavoDeodato <gus.deodato@gmail.com>
2025-05-12 23:26:52 -0300

    musica_genero

---
+++ controler/musica_genero/ControllerMusica_Genero.js
@@ -0,0 +1,175 @@
+/*********************************************
+ * Objetivo: Controller referente as ações de CRUD de genero 
+ * Data: 29/04/2025
+ * Autor: Gustavo Deodato
+ * Versão 1.0
+ * ********************************************** */
+
+//funções ainda n corrigidas 
+
+const message = require('../../modulo/config.js')
+
+const generoDAO = require('../../model/DAO/musica_genero.js')
+
+//função para inserir uma genero 
+const inserirMusicaGenero = async function (genero, contentType){
+    try {
+        if(String(contentType).toLowerCase() == 'application/json'){
+            if(genero.nome == ''|| genero.nome == null||genero.nome == undefined|| genero.nome.Length > 100 
+
+            ){
+                return message.ERROR_REQUIRED_FIELDS
+            }else{
+                //encaminhar dados da genero para o db 
+                let resultGenero = await generoDAO.inserirGenero(genero)
+
+                if(resultGenero)
+                    return message.SUCESS_CREATED_ITEM//201
+                else 
+                    return message.ERROR_INTERNAL_SERVER_MODEL///500
+            }}else{
+                        return message.ERROR_CONTENT_TYPE          
+        }
+    } catch (error) {
+        return message.ERROR_INTERNAL_SERVER_CONTROLLER//500
+    }
+}
+
+//função para atualizar uma genero 
+const atualizarMusicaGenero = async function  (){
+    try {
+        if(String(contentType).toLowerCase() == 'application/json'){
+            if(genero.nome == '' || genero.nome == null || genero.nome == undefined || genero.nome.length > 100 || 
+                id == '' || id == null || id == undefined || isNaN(id)
+                 
+            ){
+                return message.ERROR_REQUIRED_FIELDS
+            }else{
+                //verificação existancia ID no BD
+                    let result = await generoDAO.selectByIdGenero(id)
+
+                    if(result != false || typeof(result) == 'object'){
+                        if(result.length > 0 ){
+                            //Update 
+
+                            //adciona o atributi do id no json com os dados recebidos no corpo da requisição 
+                            musica.id = id 
+                                let resultgenero = await generoDAO.updateGenero(genero)
+                                if(resultgenero){
+                                    return message.SUCESS_UPDATE_ITEM
+                                }else{
+                                    return message.ERROR_INTERNAL_SERVER_MODEL//500
+                                }
+                        }else{
+                            return message.ERROR_NOT_FOUND//404
+                        }
+                    }
+            }
+        }else{
+            return message.ERROR_CONTENT_TYPE//415
+        }
+    } catch (error) {
+        return message.ERROR_INTERNAL_SERVER_CONTROLLER
+    }
+}
+
+//função para deletar uma Genero
+const excluirMusicaGenero = async function (id){
+
+    try {
+          if(id == '' || id == null || id == undefined || isNaN(id)){
+                    return message.ERROR_REQUIRED_FIELDS//400
+          }else{
+             
+            let resultGenero = generoDAO.selectByIdGenero(id)
+
+            if(resultGenero != false || typeof(resultGenero) == 'object'){
+                if(resultGenero.length > 0){
+                    //delete
+
+                    result = generoDAO.deleteGenero(id)
+
+                    if(result)
+                        return message.SUCESS_DELETE_ITEM//200
+                    else 
+                    return message.ERROR_INTERNAL_SERVER_MODEL//500
+                }else{
+                    return message.ERROR_NOT_FOUND//404
+                }
+
+            }else{
+                return message.ERROR_INTERNAL_SERVER_CONTROLLER//500
+            }
+          }
+
+
+
+    } catch (error) {
+        return message.ERROR_INTERNAL_SERVER_CONTROLLER//500
+    }
+
+}
+
+// função para mostrar todas as Genero
+const listarMusicaGenero = async function (){
+
+    try {
+        let dadosGenero = {}
+
+        let resultGenero = await generoDAO.selectAllGenero()
+
+         if(resultGenero != false || typeof(resultGenero) == 'object' ){
+                    if(resultGenero.length > 0){
+                        //cria um json para colocar o array de Genero
+                        dadosGenero.status = true
+                        dadosGenero.status_code = 200,
+                        dadosGenero.items = resultGenero.length
+                        dadosGenero.generos = resultGenero
+                        return dadosGenero
+                    }else{
+                        return message.ERROR_NOT_FOUND //404
+                    }
+                }else{
+                    return message.ERROR_INTERNAL_SERVER_MODEL//500
+                }
+        
+    } catch (error) {
+        return message.ERROR_INTERNAL_SERVER_CONTROLLER//500
+    }
+
+}
+//função para buscar uma Genero pelo ID 
+const buscarMusicaGenero = async function (id){
+    try {
+
+         if(id == '' || id == undefined || id == null || isNaN(id)){
+                   return message.ERROR_REQUIRED_FIELDS //400
+                }else{
+                    let dadosGenero = {}
+
+                    resultGenero = await GeneroDAO.selectByIdGenero(id)
+
+                        if(resultGenero != false || typeof(resultGenero) == 'object'){
+                            if(resultGenero.length > 0 ){
+                            //criacao do json para o array das Genero
+                            dadosGenero.status = true,
+                            dadosGenero.status_code = 200, 
+                            dadosGenero.genero = resultGenero
+                            return dadosGenero
+                            }else{
+                               return message.ERROR_NOT_FOUND//404
+                            }                           
+                        }else{
+                            return message.ERROR_INTERNAL_SERVER_MODEL//500
+                        }
+
+
+                }
+    } catch (error) {
+        return message.ERROR_INTERNAL_SERVER_CONTROLLER//500
+}
+}
+
+module.exports = {
+  
+}
---
+++ model/DAO/musica_genero.js
@@ -0,0 +1,109 @@
+/*********************************************
+ * Objetivo: criar o CRUD de dados da tabela genero no banco de dados 
+ * Data: 29/04/2025
+ * Autor: Gustavo Deodato
+ * Versão 1.0
+ * ********************************************** */
+
+//funções ainda não corrigidas 
+
+
+//import biblioteca prisma client para realizar as ações do db
+const {PrismaClient} = require('@prisma/client')
+
+//Instancia da classe do prisma client (cria um objeto)
+const prisma = new PrismaClient
+
+const inserirMusicaGenero = async function (genero) {
+    try {
+        let sql = `insert into tbl_genero (nome)
+        
+        values('${genero.nome}')`
+
+    //Executa o script sql no banco de dados e aguarda o resultado (retornando true ou false)
+    let result = await prisma.$executeRawUnsafe(sql)
+
+
+    if(result)
+        return true 
+    else 
+    return false 
+
+    } catch (error) {
+        console.log(error)
+        return false 
+    }
+}
+
+//Função para atualizar uma genero existente 
+const updateMusicaGenero = async function (){
+    try {
+        let sql = `update tbl_genero set nome = '${genero.nome}'
+        where id = '${genero.id}'`
+
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result)
+            return true 
+        else 
+            return false 
+        
+    } catch (error) {
+        return false 
+    }
+}
+
+
+//função para deletar uma genero 
+const deleteMusicaGenero = async function (){
+    try {
+        let sql = `delete from tbl_genero where id = ${id}`
+
+        result = await prisma.$executeRawUnsafe(sql)
+    } catch (error) {
+        
+    }
+
+}
+
+//função para mostrar todas as genero 
+const selectAllMusicaGenero = async function (){
+    try {
+        sql = `select * from tbl_genero order by id desc`
+
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(result)
+            return result
+        else
+            return false
+    } catch (error) {
+        return false 
+    }
+}
+
+//função para busca pelo ID 
+const selectByIdMusicaGenero = async function (id){
+    try {
+        let sql = `select * from tbl_genero where id = ${id}`
+
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(result)
+            return result
+        else 
+          return false
+
+    } catch (error) {
+        return false 
+    }
+    }
+
+module.exports = {
+    inserirMusicaGenero,
+    deleteMusicaGenero,
+    selectByIdMusicaGenero,
+    selectAllMusicaGenero,
+    updateMusicaGenero
+
+}
